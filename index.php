d8b756dc2e2b16ee8380a62bc898b2ed74a46d3b
blha303 <stevensmith.ome@gmail.com>
2013-11-11 18:46:29 +0800

    Github ribbon

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $sites = array("all", "chiptune", "cover", "game", "ocr");
 ?>
+<a href="https://github.com/blha303/Rainwave-NowPlaying"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
 <h2>Bookmarklets:</h2>
 <ul>
 <?php foreach ($sites as $site) { ?>
